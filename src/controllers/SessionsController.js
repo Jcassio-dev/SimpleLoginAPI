deb6166ac0f469d5580eb4535001a74247741e16
José Cássio <josecassio2013@gmail.com>
2023-07-21 09:26:12 -0300

    add user validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,17 @@
+const sqliteConnection = require("../database/sqlite");
+const AppError = require("../utils/AppError")
+
 class SessionController {
     async create(request, response){
+        const {email, password} = request.body;
 
+        const database = await sqliteConnection();
+        const user = await database.get('SELECT * FROM users WHERE email=(?)', [email])
+        
+        if(!user){
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        }
+        return response.json(user)
     }
 }
 
