24de070a73777f02f8558b3d1269a2357fef5e6a
José Cássio <josecassio2013@gmail.br>
2023-07-21 08:25:04 -0300

    add users routes

--- src/server.js
+++ src/server.js
@@ -4,5 +4,7 @@ const routes = require("./routes")
 const app = express()
 app.use(express.json());
 
+app.use(routes);
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
