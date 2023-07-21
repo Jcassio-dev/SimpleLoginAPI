eb72d85679b6667ff8b83e9f72c971fb3824a888
José Cássio <josecassio2013@gmail.com>
2023-07-21 09:02:53 -0300

    create register in controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,14 +1,22 @@
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+const {hash, compare} = require("bcryptjs");
 
 class UsersController{
     async create(request, response){
         const {name, email, password} = request.body;
 
-        if(!nome){
-            throw new AppError("O nome é obrigatório!", 401);
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get('SELECT * FROM users WHERE email=(?)', [email])
+        if(checkUserExist){
+            throw new AppError("Este E-mail já está em uso")
         }
 
-        response.status(201).json({name, email, password});
+        const hashedPassword = await hash(password, 8);
+
+        await database.run("INSERT INTO users(name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+        return response.status(201).json(`Usuário Cadastrado!`)
     }
 }
 
