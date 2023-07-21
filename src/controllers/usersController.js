e5ec6065684d916e0b3e552a51609ce989cf9e52
José Cássio <josecassio2013@gmail.br>
2023-07-21 08:37:31 -0300

    add apperror

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UsersController{
     async create(request, response){
         const {name, email, password} = request.body;
+
+        if(!nome){
+            throw new AppError("O nome é obrigatório!", 401);
+        }
 
         response.json({name, email, password});
     }
